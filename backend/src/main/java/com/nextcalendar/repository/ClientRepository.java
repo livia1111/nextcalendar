dc7980935ec1c928818537570f2d9c840258d905
Gabrielli Borba <gabrielliborba42@gmail.com>
2026-07-11 15:25:27 -0300

    feat(clientRepository): adiciona estrutura de buscas iniciais por email, cpf e nome

---
+++ backend/src/main/java/com/nextcalendar/repository/ClientRepository.java
@@ -0,0 +1,17 @@
+package com.nextcalendar.repository;
+
+import com.nextcalendar.entity.ClientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ClientRepository extends JpaRepository<ClientEntity, UUID> {
+
+    Optional<ClientEntity> findByEmail(String email);
+    Optional<ClientEntity> findByCpf(String cpf);
+
+    List<ClientEntity> findByNameContainingIgnoreCase(String name);
+
+}

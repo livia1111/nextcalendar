43ffe9a8351b1bb6037e8e370b557fb975119075
Gabrielli Borba <gabrielliborba42@gmail.com>
2026-07-11 10:35:12 -0300

    feat(technicalSheetEntity): cria entidade TechnicalSheet e seus atributos

--- backend/src/main/java/com/nextcalendar/entity/TechnicalSheetEntity.java
+++ backend/src/main/java/com/nextcalendar/entity/TechnicalSheetEntity.java
@@ -1,4 +1,44 @@
 package com.nextcalendar.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "technical_sheets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class TechnicalSheetEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", nullable = false, unique = true)
+    private ClientEntity client;
+
+    @Column(columnDefinition = "TEXT")
+    private String observations;
+
+    //private List<ServicePhoto> photos;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 }

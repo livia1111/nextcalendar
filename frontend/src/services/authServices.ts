3d0956164cc10072d920ea235e40a561939850ff
Pedro Marcolino <pedromathiasdasilvamarcolino14@gmail.com>
2026-07-24 02:16:40 -0300

    feat(auth): add register function to authServices

--- frontend/src/services/authServices.ts
+++ frontend/src/services/authServices.ts
@@ -1,4 +1,4 @@
-// src/services/authService.ts
+// src/services/authServices.ts
 
 type LoginResponse = {
   token: string;
@@ -8,6 +8,23 @@ type LoginResponse = {
     email: string;
   };
 };
+
+type RegisterPayload = {
+  name:string;
+  email:string;
+  password:string;
+  phone?:string;
+}
+
+type RegisterResponse = {
+  token: string;
+  user :{
+    id:string;
+    name:string;
+    email:string;
+    phone?:string;
+  }
+}
 
 // 🔧 MOCK TEMPORÁRIO — trocar pela chamada real quando o back estiver pronto
 export async function login(email: string, password: string): Promise<LoginResponse> {
@@ -27,4 +44,21 @@ export async function login(email: string, password: string): Promise<LoginRespo
       email,
     },
   };
+}
+
+export async function register(data:RegisterPayload): Promise<RegisterResponse>{
+
+  await new Promise((resolve)=> setTimeout(resolve,1000))
+
+  if(data.email===' ja-existe@teste.com'){
+    throw {message:'Este e-mail ja esta cadastrado'}
+  }
+  return {
+    token:'mock-jwt-fake',
+    user:{
+      id:'mock-data',
+      name:data.name,
+      email:data.email
+    }
+  }
 }
